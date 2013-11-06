fb86e377d79b727181272952e52202889b133312
Raghu Betina <rbetina@gmail.com>
2013-11-05 22:35:00 -0600

    rails new flashcards

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Flashcards::Application.config.secret_key_base = 'f6362f62c1441c7e0abd63e01e11981e792cea18096a997461aab9f0943ee30a92af5f7a7c41189ae504aa39a8914fd6ae6d50bf1fbbc96a945f6f064a6599f1'
